3ed954116668dca80c253bae1c04e6c25f2dab9f
Neerajkhare <neeraj.khare980@gmail.com>
2018-10-16 13:13:47 +0530

    Check in all project in git hub

---
+++ src/test/java/org/o7plannng/sbshoppingcart/SbHibernateShoppingCartApplicationTests.java
@@ -0,0 +1,17 @@
+package org.o7plannng.sbshoppingcart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+public class SbHibernateShoppingCartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
